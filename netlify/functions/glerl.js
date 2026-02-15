aced84f4529305e908caaf928b069e9fe003f332
Edward Mui <edward.k.mui@gmail.com>
2026-02-15 13:52:54 -0600

    Fix glerl function syntax

--- netlify/functions/glerl.js
+++ netlify/functions/glerl.js
@@ -1,34 +1,33 @@
 const https = require('https');
 
-exports.handler = async function(event) {
+exports.handler = function(event, context, callback) {
   const today = new Date();
   const y  = today.getUTCFullYear();
   const mm = String(today.getUTCMonth() + 1).padStart(2, '0');
   const dd = String(today.getUTCDate()).padStart(2, '0');
-  const url = `https://www.glerl.noaa.gov/metdata/chi/${y}/${y}${mm}${dd}.04t.txt`;
+  const url = 'https://www.glerl.noaa.gov/metdata/chi/' + y + '/' + y + mm + dd + '.04t.txt';
 
-  return new Promise((resolve) => {
-    https.get(url, (res) => {
-      let data = '';
-      res.on('data', chunk => data += chunk);
-      res.on('end', () => {
-        resolve({
-          statusCode: 200,
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'text/plain',
-            'Cache-Control': 'no-cache'
-          },
-          body: data
-        });
+  https.get(url, function(res) {
+    let data = '';
+    res.on('data', function(chunk) {
+      data += chunk;
+    });
+    res.on('end', function() {
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'text/plain',
+          'Cache-Control': 'no-cache'
+        },
+        body: data
       });
-    }).on('error', (err) => {
-      resolve({
-        statusCode: 500,
-        headers: { 'Access-Control-Allow-Origin': '*' },
-        body: `Error: ${err.message}`
-      });
+    });
+  }).on('error', function(err) {
+    callback(null, {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: 'Error: ' + err.message
     });
   });
 };
-```
